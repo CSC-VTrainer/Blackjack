a638243ce37b621aa33f403b289d23707f5ef94f
Vladlena <78901460+adlen-create@users.noreply.github.com>
2021-03-12 23:47:12 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,22 +1,9 @@
-#include <iostream>
-#include "Dealer.h"
-#include "Card.h"
-#include "AbstractPlayer.h"
+#include "GameManager.h"
 
 using namespace std;
 
 int main() {
-    Dealer a;
-    a.shuffle_deck();
-    a.shuffle_deck();
-    a.shuffle_deck();
-    AbstractPlayer p;
-
-    p.receive_card(a.give_card());
-    p.receive_card(a.give_card());
-
-    p.print_hand();
-    a.print_deck();
+    GameManager a;
 
 	return 0;
 }
