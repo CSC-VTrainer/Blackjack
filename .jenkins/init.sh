4ec47202d0e8d59f67e715635728d0b6e76707eb
AlexKaravaev <alexkaravev@gmail.com>
2021-05-02 00:03:44 +0300

    update python to python3

--- .jenkins/init.sh
+++ .jenkins/init.sh
@@ -1,5 +1,4 @@
 curl -O https://bootstrap.pypa.io/pip/2.7/get-pip.py
 sudo apt-get update && apt-get install -y apt-transport-https
-sudo apt-get install python
-sudo python get-pip.py
-sudo python -m pip install -r ./web/requirements.txt
+sudo apt-get install python3 python3-pip
+sudo python3 -m pip install -r ./web/requirements.txt
