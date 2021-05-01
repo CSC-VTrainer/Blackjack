d68872341df1cfa345596ca738163d088a8cb2fe
AlexKaravaev <alexkaravev@gmail.com>
2021-05-02 01:53:35 +0300

    fix buildv16

--- .jenkins/init.sh
+++ .jenkins/init.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 curl -O https://bootstrap.pypa.io/pip/2.7/get-pip.py
 sudo apt-get update && apt-get install -y apt-transport-https
 sudo apt-get update && sudo apt-get install -y python3 python3-pip
--- .jenkins/start.sh
+++ .jenkins/start.sh
@@ -1,1 +1,2 @@
+#!/bin/bash
 cd web && python app.py > /dev/null 2>&1 &
