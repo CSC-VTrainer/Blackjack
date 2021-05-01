6fd2550b6af54b420e1ae0b751e0ccbeee9c314b
AlexKaravaev <alexkaravev@gmail.com>
2021-05-02 00:22:01 +0300

    fix buildv6

--- .jenkins/init.sh
+++ .jenkins/init.sh
@@ -1,4 +1,4 @@
 curl -O https://bootstrap.pypa.io/pip/2.7/get-pip.py
 sudo apt-get update && apt-get install -y apt-transport-https
-sudo apt-get install python3 python3-pip
+sudo apt-get update && sudo apt-get install -y python3 python3-pip
 sudo python3 -m pip install -r ./web/requirements.txt
