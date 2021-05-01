5febc495e06bd601826b02c5b5a5b2a876a5b043
AlexKaravaev <alexkaravev@gmail.com>
2021-05-02 02:05:03 +0300

    build-fixv1222222

--- .jenkins/init.sh
+++ .jenkins/init.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 curl -O https://bootstrap.pypa.io/pip/2.7/get-pip.py
-sudo apt-get update && apt-get install -y apt-transport-https
-sudo apt-get update && sudo apt-get install -y python3 python3-pip
-sudo python3 -m pip install -r ./web/requirements.txt
+apt-get update && apt-get install -y apt-transport-https
+apt-get update && apt-get install -y python3 python3-pip
+python3 -m pip install -r ./web/requirementstxt
